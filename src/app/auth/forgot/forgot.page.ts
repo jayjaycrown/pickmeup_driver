8ab118cfdd7dbf7b501e05ee47a6b9f849725a61
JONATHAN SOLAGBADE <olajidecrown@gmail.com>
2021-07-10 15:05:36 +0100

    Minor fixes

--- src/app/auth/forgot/forgot.page.ts
+++ src/app/auth/forgot/forgot.page.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
-import { AuthService } from '../../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AlertController, LoadingController } from "@ionic/angular";
+import { AuthService } from "../../_services/auth.service";
 
 @Component({
-  selector: 'app-forgot',
-  templateUrl: './forgot.page.html',
-  styleUrls: ['./forgot.page.scss'],
+  selector: "app-forgot",
+  templateUrl: "./forgot.page.html",
+  styleUrls: ["./forgot.page.scss"],
 })
 export class ForgotPage implements OnInit {
   forgotForm: any;
@@ -18,42 +18,47 @@ export class ForgotPage implements OnInit {
     private loadingController: LoadingController,
     private alertController: AlertController,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.forgotForm = this.formBuilder.group({
-      email: ['', Validators.compose([Validators.maxLength(30), Validators.required, Validators.email])],
-      password: ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      email: [
+        "",
+        Validators.compose([
+          Validators.maxLength(30),
+          Validators.required,
+          Validators.email,
+        ]),
+      ],
+      // password: ['', Validators.compose([Validators.minLength(6), Validators.required])],
     });
   }
   async verify() {
-    console.log(this.forgotForm.value);
+    // console.log(this.forgotForm.value);
     const loading = await this.loadingController.create({
       spinner: null,
-      cssClass: 'custom-loading'
+      cssClass: "custom-loading",
     });
     await loading.present();
     this.auth.forgot(this.forgotForm.value).subscribe(async (res: any) => {
       if (res.success === false) {
         await loading.dismiss();
         const alert = await this.alertController.create({
-          header: 'Verification Failed',
+          header: "Verification Failed",
           message: res.message,
-          buttons: ['OK'],
+          buttons: ["OK"],
         });
         await alert.present();
       } else {
         await loading.dismiss();
         const alert = await this.alertController.create({
-          header: 'Verification Successfull',
+          header: "Verification Successfull",
           message: res.message,
-          buttons: ['OK'],
+          buttons: ["OK"],
         });
         await alert.present();
-        this.router.navigateByUrl('/auth/reset');
+        this.router.navigateByUrl("/auth/reset");
       }
     });
-
   }
-
 }
--- src/app/auth/reset/reset.page.ts
+++ src/app/auth/reset/reset.page.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AlertController, LoadingController } from '@ionic/angular';
-import { AuthService } from '../../_services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+import { AlertController, LoadingController } from "@ionic/angular";
+import { AuthService } from "../../_services/auth.service";
 
 @Component({
-  selector: 'app-reset',
-  templateUrl: './reset.page.html',
-  styleUrls: ['./reset.page.scss'],
+  selector: "app-reset",
+  templateUrl: "./reset.page.html",
+  styleUrls: ["./reset.page.scss"],
 })
 export class ResetPage implements OnInit {
   resetForm: any;
@@ -18,45 +18,45 @@ export class ResetPage implements OnInit {
     private loadingController: LoadingController,
     private alertController: AlertController,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.resetForm = this.formBuilder.group({
-      resetToken: ['', Validators.compose([Validators.required])],
-      newPassword: ['', Validators.compose([Validators.minLength(6), Validators.required])],
+      resetToken: ["", Validators.compose([Validators.required])],
+      newPassword: [
+        "",
+        Validators.compose([Validators.minLength(6), Validators.required]),
+      ],
     });
   }
 
-
-
   async reset() {
-    console.log(this.resetForm.value);
+    // console.log(this.resetForm.value);
     const loading = await this.loadingController.create({
       spinner: null,
 
-      cssClass: 'custom-loading'
+      cssClass: "custom-loading",
     });
     await loading.present();
     this.auth.reset(this.resetForm.value).subscribe(async (res: any) => {
       if (res.success === false) {
         await loading.dismiss();
         const alert = await this.alertController.create({
-          header: 'Failed',
+          header: "Failed",
           message: res.message,
-          buttons: ['OK'],
+          buttons: ["OK"],
         });
         await alert.present();
       } else {
         await loading.dismiss();
         const alert = await this.alertController.create({
-          header: 'Successfull',
+          header: "Successfull",
           message: res.message,
-          buttons: ['OK'],
+          buttons: ["OK"],
         });
         await alert.present();
-        this.router.navigateByUrl('/auth');
+        this.router.navigateByUrl("/auth");
       }
     });
   }
-
 }
